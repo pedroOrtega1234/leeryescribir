67b027ccd128152b292387898c0a65319acb0075
pedroOrtega1234 <128636704+pedroOrtega1234@users.noreply.github.com>
2023-03-22 14:14:31 -0600

    Add files via upload
    cree programador de correos

---
+++ correos.py
@@ -0,0 +1,20 @@
+#este programa crea cerros
+#lista de nombres y apellidos
+nombres=["adrian","francisco","sarai","sergio","aurelio"]
+apellidos=["hernandez","perez","patricio","lezama","hernandez"]
+correos=[]
+#checar que sean de igual tamaño
+ln=len(nombres)
+la=len(apellidos)
+if ln==la:
+    #abrir archivo
+    lista=open("lista.txt","w")
+    for i in range(ln):
+        correo=nombres[i]+"."+apellidos[i]+"@correo.com"
+        correos.append(correo)
+        print(correo)
+    for x in range(ln):
+        lista.write(correos[x])
+        lista.write("\n")
+    lista.close()
+            
